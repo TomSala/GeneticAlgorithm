b771ef2897f0e3501a6a3a385382db0a2c113ffb
TomSala <32878364+TomSala@users.noreply.github.com>
2021-06-08 21:32:41 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,25 +3,41 @@
 
 from music21 import *
 import random
+import time
+import os
+from valutatore import calcolo_fitness
 
-N_STEP = 5
+N_STEP = int(input("Numero di generazioni: "))
+CHECKPOINT = N_STEP / 10
 
-scala = ['C4','C#4','D','D#4','E','F','F#','G','G#','A','A#','B']
-musica = [0, 2, 4, 0, 0, 2, 4, 0, 4, 5, 7, 4, 5, 7, 7, 9]
-musica2 = [0, 2, 4, 0, 0, 2, 4, 0, 4, 5, 7, 4, 5, 7, 7, 9]
+#scala = ['C4','C#4','D','D#4','E','F','F#','G','G#','A','A#','B']
+scala = []
+lista_note = ['C','C#','D','D#','E','F','F#','G','G#', 'A','A#','B']
+for i in range(3,6):
+    for a in lista_note:
+        nota = a + str(i)
+        scala.append(nota)
+durate = [1/16]
+INIZIO = time.time()
 
 def genera_lista():
     lista = []
     for i in range(0, 16):
-        lista.append(random.randint(0,11))
+        nota = random.randint(0,len(scala)-1)
+        durata = random.randint(0,2)
+        lol = [nota, durata]
+        lista.append(lol) # <----- TUPLA !! LISTA
     return lista
 
-def esporta(sequenza, nome_file):
-    scala = ['C4','C#4','D','D#4','E','F','F#','G','G#','A','A#','B']
+def esporta(sequenza, nome_file, scala=scala):
     stream1 = stream.Stream()
     for nota in sequenza:
-        stream1.append(note.Note(scala[nota]))
-    stream1.write('midi', fp='T://datasets//musica//'+nome_file)
+        pos = nota[0]
+        dur = nota[1]
+        nota = note.Note(scala[pos])
+        nota.quarterLength = int(dur)
+        stream1.append(nota)
+    stream1.write('midi', fp='cache_test//'+nome_file)
 
 def crossover(lista1, lista2):
     lista1 = lista1.copy()
@@ -38,31 +54,31 @@ def mutazione(lista):
     segno_mutazione = random.randint(0,1)
     p_mutazione = random.randint(0,15)
     if segno_mutazione == 1:
-        if lista[p_mutazione] == 11:
-            lista[p_mutazione] -= 1
+        if lista[p_mutazione][0] == len(scala)-1:
+            lista[p_mutazione][0] -= 1
         else:
-            lista[p_mutazione] += 1
+            lista[p_mutazione][0] += 1
     if segno_mutazione == 0:
-        if lista[p_mutazione] == 0:
-            lista[p_mutazione] += 1
+        if lista[p_mutazione][0] == 0:
+            lista[p_mutazione][0] += 1
         else:
-            lista[p_mutazione] -= 1
+            lista[p_mutazione][0] -= 1
     return lista
 
+
 def genitori(popolazione, fitness):
+    tupla = []
+    for i in range(0, len(popolazione)):
+        tupla.append((fitness[i], popolazione[i]))
     def genitore(popolazione, fitness):
         poss_gen = []
-        for i in range(0,3):
+        for i in range(0,2):
             poss = random.randint(0,9)
-            poss_gen.append(poss)
-        valori_fit = []
-        for scelta in poss_gen:
-            valori_fit.append(fitness[scelta])
-        valori_fit.sort()
-        valori_fit.reverse()
-        pos_gen = valori_fit[0]
-        pos_gen = fitness.index(pos_gen)
-        gen = popolazione[pos_gen] #popolazione[pos_gen1]
+            poss_gen.append(tupla[poss])
+
+        poss_gen.sort()
+        poss_gen.reverse()
+        gen = poss_gen[0][1]
         return gen
     gen1 = genitore(popolazione, fitness)
     gen2 = genitore(popolazione, fitness)
@@ -83,53 +99,70 @@ def elitarismo(popolazione1, popolazione2, fitness1, fitness2):
     del popolazione2[indice_peggiore]
     del fitness2[indice_peggiore]
     sequenza_migliore = popolazione1[indice_migliore]
-    popolazione1.append(sequenza_migliore)
+    popolazione2.append(sequenza_migliore)
     fitness2.append(migliore)
     return popolazione2, fitness2
 
 #STEP 0: creazione popolazione di partenza e associazione fitness
 popolazione = []
-nomi_esportazione = []
 
 for i in range(0,10):
     popolazione.append(genera_lista())
-
-for i, m in enumerate(popolazione):
-    filename = str('m0 '+str(i)+'.midi')
-    nomi_esportazione.append(filename)
-    esporta(m, filename)
-
+#print(popolazione)
 fitness = []
-
-for i, m in enumerate(nomi_esportazione):
-    msg = str('Fitness ' + str(m) + ' ')
-    valore_fitness = input(str(msg))
+x = True
+for melodia in popolazione:
+    if x == True:
+        print(melodia)
+        x = False
+    valore_fitness = calcolo_fitness(melodia)
     fitness.append(valore_fitness)
 
 
 #STEP 1: elaborazione popolazione1 e associazione fitness1
+ora = time.time()
+
 for step in range(0, N_STEP):
     popolazione1 = []
-    fitness1 = []
+    test_gen = []
+    test_children = []
     for i in range(0,5):
         gen1, gen2 = genitori(popolazione, fitness)
+        test_gen.append(gen1)
+        test_gen.append(gen2)
         c1, c2 = crossover(gen1, gen2)
         c1 = mutazione(c1)
         c2 = mutazione(c2)
+        test_children.append(c1)
+        test_children.append(c2)
         popolazione1.append(c1)
         popolazione1.append(c2)
-        
-    nomi_esportazione = []
-    for i, m in enumerate(popolazione1):
-        filename = 'm'+str(step+1)+' '+str(i)+'.midi'
-        nomi_esportazione.append(filename)
-        esporta(m, filename)
 
-    for i, m in enumerate(nomi_esportazione):
-        msg = str('Fitness ' + str(m) + ' ')      
-        valore_fitness = input(str(msg))
+    fitness1 = []
+
+    for melodia in popolazione:
+        valore_fitness = calcolo_fitness(melodia)
         fitness1.append(valore_fitness)
+
     
     popolazione1, fitness1 = elitarismo(popolazione, popolazione1, fitness, fitness1)
+
     popolazione = popolazione1
     fitness = fitness1
+
+    if step%1000 == 0:
+        print("Generazione numero", step)
+        print(time.time()-ora)
+        ora = time.time()
+        print()
+    if step%CHECKPOINT == 0 or step > N_STEP-2:
+        for i, m in enumerate(popolazione1):
+            filename = 'm'+str(step+1)+' '+str(i)+'.midi'
+            esporta(m, filename)
+                
+    
+
+TEMPO_TOTALE = time.time()-INIZIO
+print("Ho impiegato "+str(TEMPO_TOTALE)+" secondi per "+str(N_STEP)+" step!")
+print("Media: "+str(TEMPO_TOTALE/N_STEP))
+
--- valutatore.py
+++ valutatore.py
@@ -1,19 +1,47 @@
 import random
 import math
 from music21 import *
+
+'''
 int_chart = {'0': ('2', '9', '0'), '2': ('4', '2', '0'),
              '4': ('4', '5', '2'), '5': ('7', '4'),
              '7': ('5', '1', '7'), '9': ('0', '7', '9')}
+'''
+#Interval Distributions, Mode, and Tonal Strength of Melodies as Predictors of Perceived Emotion
 
+main_chart = {'1': 12.04, '-1': 14.86, '2': 12.91,
+         '-2': 17.64, '3': 5.38, '-3':4.53, '4':2.77, '-4': 3.55,
+         '5': 4.24, '-5': 2.69}
 scala = ['C4','C#4','D','D#4','E','F','F#','G','G#','A','A#','B']
 
-    
+
+
+def ascdesc(melodia, chart=main_chart):
+    moltiplicatore = 1
+    for i in range(0,len(melodia)-1):
+        s = i + 1
+        delta = melodia[s]-melodia[i]
+        if delta != 0 and delta in range(-5,6):
+            delta = str(delta)
+            moltiplicatore *= chart[delta]
+        elif delta == 0:
+            moltiplicatore *= 2
+        else:
+            moltiplicatore *= 0.3
+    return math.sqrt(math.sqrt(moltiplicatore))
+
+'''    
 def frequenza(f_melodia):
     c_d = 0
     for i in f_melodia:
         if i == 0 or i == 2:
             c_d += 1
     return c_d
+
+def calcolo_fitness(melodia):
+    delta_lista
+    for i in range(len(melodia)):
+        delta = abs(melodia[i]-melodia[i+1])
 
 
 def calcolo_fitness(melodia):
@@ -28,21 +56,21 @@ def calcolo_fitness(melodia):
                 pass
                 
     return moltiplicatore + indice
-
+'''
 
 
 for n in range(0,10):
     melodia = []
-    for i in range(0,16):
+    for i in range(0,10):
         melodia.append(random.randint(0,11))
 
-    moltiplicatore = calcolo_fitness(melodia)
-    print(moltiplicatore)
+    moltiplicatore = math.sqrt(math.sqrt(ascdesc(melodia)))
     stream1 = stream.Stream()
     for nota in melodia:
         stream1.append(note.Note(scala[nota]))
-    stream1.write('midi', fp='cache_test//'+str(n)+'.midi')
-    moltiplicatore = 1
+    stream1.write('midi', fp='T://datasets//musica//'+str(n)+".midi")
+    print(moltiplicatore)
+
     
 
 
